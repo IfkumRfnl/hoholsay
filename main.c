bd9a0c199b3052c6c5b355d0f4cfbad425b4f70f
IfkumRfnl <ifkumrfnl@gmail.com>
2023-01-04 15:42:15 +0400

    stdin

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, char* argv[]) {
-	printf("< ");
+	if (argc <= 1) {
+		char c[10000];
+		fgets(c, 10000, stdin);
+		int len = strlen(c);
+		if (len > 0 && c[len - 1] == '\n') {
+			c[len - 1] = '\0';
+		}
+		printf("< %s", c);
+	}
+	else printf("< ");
 	for (int i = 1; i < argc; i++) {
 		printf("%s ", argv[i]);
 	}
-	printf(">\n    \\     ┏━╮╭━┓       \n     \\    ┃┏┗┛┓┃      \n      \\   ╰┓▋▋┏╯       \n         ╭━┻╮╲┗━━━━╮╭╮ \n         ┃▎▎┃╲╲╲╲╲╲┣━╯ \n         ╰━┳┻▅╯╲╲╲╲┃   \n           ╰━┳┓┏┳┓┏╯   \n             ┗┻┛┗┻┛    \n");
+	printf(" >\n    \\     ┏━╮╭━┓       \n     \\    ┃┏┗┛┓┃      \n      \\   ╰┓▋▋┏╯       \n         ╭━┻╮╲┗━━━━╮╭╮ \n         ┃▎▎┃╲╲╲╲╲╲┣━╯ \n         ╰━┳┻▅╯╲╲╲╲┃   \n           ╰━┳┓┏┳┓┏╯   \n             ┗┻┛┗┻┛    \n");
 	return 0;
 }
