db66e392e7782bd6afd1ce84a3d318af7ec8089b
IfkumRfnl <ifkumrfnl@gmail.com>
2023-01-04 16:14:04 +0400

    hotfix

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 int main(int argc, char* argv[]) {
 	if (argc <= 1) {
-		char c[10000];
-		fgets(c, 10000, stdin);
-		int len = strlen(c);
-		if (len > 0 && c[len - 1] == '\n') {
-			c[len - 1] = '\0';
+		char* str = calloc(100, sizeof(char));
+		char c = getchar();
+		int capacity = 100;
+		int len = 0;
+		while (c != EOF) {
+			if (len == capacity) {
+				capacity *= 2;
+				str = realloc(str, capacity);
+			}
+			str[len] = c;
+			len++;
+			c = getchar();
 		}
-		printf("< %s", c);
+		len = strlen(str);
+		str[len-1] = '\0';
+		printf(" <%s ", str);
+		free(str);
 	}
 	else printf("< ");
 	for (int i = 1; i < argc; i++) {
 		printf("%s ", argv[i]);
 	}
-	printf(" >\n    \\     ┏━╮╭━┓       \n     \\    ┃┏┗┛┓┃      \n      \\   ╰┓▋▋┏╯       \n         ╭━┻╮╲┗━━━━╮╭╮ \n         ┃▎▎┃╲╲╲╲╲╲┣━╯ \n         ╰━┳┻▅╯╲╲╲╲┃   \n           ╰━┳┓┏┳┓┏╯   \n             ┗┻┛┗┻┛    \n");
+	printf(">\n    \\     ┏━╮╭━┓       \n     \\    ┃┏┗┛┓┃      \n      \\   ╰┓▋▋┏╯       \n         ╭━┻╮╲┗━━━━╮╭╮ \n         ┃▎▎┃╲╲╲╲╲╲┣━╯ \n         ╰━┳┻▅╯╲╲╲╲┃   \n           ╰━┳┓┏┳┓┏╯   \n             ┗┻┛┗┻┛    \n");
 	return 0;
 }
